29dc7707578031230de55eb015b59433cf1496ac
Pierre Camplin <pierre.camplin@gmail.com>
2022-11-17 11:16:53 +0100

    Add files via upload

---
+++ BogoSort.py
@@ -0,0 +1,21 @@
+from random import shuffle, randint
+
+
+def is_sorted(data) -> bool:
+    """Determine whether the data is sorted."""
+    return all(a <= b for a, b in zip(data, data[1:]))
+
+
+def bogosort(data: list) -> list:
+    """ Shuffle data until sorted. """
+    while not is_sorted(data):
+        shuffle(data)
+        print(data)
+    return data
+
+
+def randomlist(length: int):
+    tab = []
+    for i in range(length):
+        tab.append(randint(0, length))
+    return tab
