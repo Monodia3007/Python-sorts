4b388d51b0a9a233e88cac12db7b3c285e883950
Redrock <pierre.camplin@gmail.com>
2022-11-17 11:22:49 +0100

    Updated main for the ability to choose the sort you use

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from BogoSort import *
 from MergeSort import merge_sort
+
+
 # This is a sample Python script.
 
 # Press Shift+F10 to execute it or replace it with your code.
@@ -14,6 +16,13 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
-    print(bogosort(randomlist(100)))
+    print("Which sort do you want to use ?\n1) Bogo sort\n2) Merge sort")
+    answer = int(input("Réponse : "))
+    print("Quelle taille de liste voulez vous ?")
+    size = int(input("Taille : "))
+    if answer == 1:
+        bogosort(randomlist(size))
+    elif answer == 2:
+        merge_sort(size)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
